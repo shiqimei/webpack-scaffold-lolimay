a01404eae97d0cb2bf078c06cd77b6c5aad61e46
lolimay <1404363070@qq.com>
2018-08-31 21:25:06 +0800

    complete build/initialize.js

--- scaffold/build/initialize.js
+++ scaffold/build/initialize.js
@@ -1,6 +1,7 @@
 const chalk = require('chalk')
 const readline = require('readline')
 const fs = require('fs')
+const path = require('path')
 
 const log = {
     info: (text) => {
@@ -15,8 +16,22 @@ const rl =readline.createInterface({
     output: process.stdout
 })
 
-log.info('请输入项目名')
+fs.readFile(path.join(__dirname, '../package.json'), 'utf8', (err, pkg) => {
+    if(err) throw err
 
+    pkg = JSON.parse(pkg)
+    const scripts = pkg.scripts
+
+    delete scripts.test
+    scripts.start = 'NODE_ENV=development webpack-dev-server --open --hot --config build/webpack.config.js'
+    scripts.build = 'NODE_ENV=production webpack --config build/webpack.config.js'
+
+    fs.writeFile(path.join(__dirname, '../package.json'), JSON.stringify(pkg, null, 4), (err) => {
+        if(err) throw err
+    })
+})
+
+log.info('请输入项目名')
 rl.on('line', (AppName) => {
     const template = `
     <!DOCTYPE html>
