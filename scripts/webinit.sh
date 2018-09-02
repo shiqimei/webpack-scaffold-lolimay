c419c0f14d4bd69dbbce4772691539f9cbd05038
lolimay <1404363070@qq.com>
2018-09-02 21:09:04 +0800

    change cnpm to npm

--- scripts/webinit.sh
+++ scripts/webinit.sh
@@ -3,6 +3,6 @@
 mkdir $1 && cd $1
 cp /media/lolimay/Code/Web/Template/webpack/scaffold/. . -r
 npm init -y
-cnpm i -D chalk
+npm i -D chalk
 node build/initialize.js
-cnpm i -D babel-eslint clean-webpack-plugin css-loader eslint html-webpack-plugin inline-manifest-webpack-plugin optimize-css-assets-webpack-plugin style-loader uglifyjs-webpack-plugin webpack webpack-dev-server webpack-merge copy-webpack-plugin
+npm i -D babel-eslint clean-webpack-plugin css-loader eslint html-webpack-plugin inline-manifest-webpack-plugin optimize-css-assets-webpack-plugin style-loader uglifyjs-webpack-plugin webpack webpack-dev-server webpack-merge copy-webpack-plugin
