65e7c049fe9d6ef71502f236fcee8940daa44aa9
lolimay <lolimay@lolimay.cn>
2019-02-17 02:12:18 +0800

    update scaffold

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import './style/style.css'
+
+const container = document.createElement('div')
+
+container.classList.add('container')
+container.innerText = 'Hello Webpack!'
+
+document.body.appendChild(container)
